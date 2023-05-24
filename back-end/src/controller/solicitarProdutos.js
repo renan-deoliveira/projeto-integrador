d1e5c9881172ed8e73a448c34ba4e863a2a9cf32
Renan de Oliveira <renan.deoliveira@edglobo.com.br>
2023-05-23 21:19:25 -0300

    adding image field to api

--- back-end/src/controller/solicitarProdutos.js
+++ back-end/src/controller/solicitarProdutos.js
@@ -1,4 +1,6 @@
 const Produtos = require('../models/produtoModel.js');
+const fs = require('fs');
+const path = require('path');
 
 async function getAllGames(req, res) {
     try {
@@ -21,7 +23,33 @@ async function addGame(req, res) {
     }
 }
 
+async function adicionarProduto(req, res) {
+    try {
+        const { name, description, price } = req.body;
+        const picture = req.file; // Imagem enviada como arquivo (usando biblioteca como Multer)
+
+        // Salvar a imagem no sistema de arquivos
+        const pictureNome = picture.filename; // Nome gerado pelo Multer
+        const pictureCaminho = path.join(__dirname, '../../uploads/', pictureNome);
+        fs.renameSync(picture.path, pictureCaminho);
+
+        // Criar o produto no banco de dados
+        const novoProduto = await Produtos.create({
+            name,
+            description,
+            price,
+            picture: pictureNome // Armazenar o nome do arquivo da imagem no banco de dados
+        });
+
+        res.status(201).json(novoProduto);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
 module.exports = {
     getAllGames,
     addGame,
+    adicionarProduto,
 };
