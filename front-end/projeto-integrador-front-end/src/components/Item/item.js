b1435248862de3e99ce4e7a8306740469405943a
Renan de Oliveira <renan.deoliveira@edglobo.com.br>
2023-05-24 22:23:54 -0300

    integrate backend and frontend

--- front-end/projeto-integrador-front-end/src/components/Item/item.js
+++ front-end/projeto-integrador-front-end/src/components/Item/item.js
@@ -1,26 +1,26 @@
 import styles from '../../styles/Home.module.css'
 
 
-export default function Item({props,index})  {
+export default function Item({ props, index }) {
 
     const rating = (number) => {
         let starRating = "";
-        for (var i = 0; i < 5 ; i++){
-            ( number <= i ? starRating += "☆" : starRating += "★");
+        for (var i = 0; i < 5; i++) {
+            (number <= i ? starRating += "☆" : starRating += "★");
         }
-        return starRating ;
+        return starRating;
     };
-    return(
+    return (
         <>
-             <div key={index} className={styles.item}> 
-                {<img src ={props.src} alt={props.alt}  className={styles.item_img}/> }
+            <div key={index} className={styles.item}>
+                {<img src={props.picture} alt={props.alt} className={styles.item_img} />}
                 <div>
-                    <h3 className={styles.item_titulo}>{props.titulo} - {props.console}</h3>
-                    <p className={styles.item_preco}>R$ {props.preco},00</p>
-                    <p className={styles.item_preco}>até {props.parcelas}x de R$ {props.valorDaParcela},00 sem juros</p>
-                    <p className={styles.item_preco}>ou R$ {(props.preco / 10) * 9}  no PIX</p>
+                    <h3 className={styles.item_titulo}>{props.name} - {props.console}</h3>
+                    <p className={styles.item_preco}>R$ {props.price},00</p>
+                    <p className={styles.item_preco}>até {props.parcelas}x de R$ {props.price / props.parcelas} sem juros</p>
+                    <p className={styles.item_preco}>ou R$ {(props.price / 10) * 9}  no PIX</p>
                 </div>
             </div>
-       </>
+        </>
     )
 }
